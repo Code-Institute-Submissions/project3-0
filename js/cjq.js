c05440174d7d95401c276ea43db561440e4340bd
Ciaran Quinlan <ciaran.quinlan@gmail.com>
2019-07-04 14:25:09 +0100

    magento connection working and returning orders

---
+++ js/cjq.js
@@ -0,0 +1,156 @@
+ //   The ready() method is used to make a function available after the document is loaded.
+ $(document).ready(function() {
+   /*
+    * get order details by order id
+    * pass the Access Token
+    * return the order details form order endpoint
+    */
+   // The change() method triggers the change event, or attaches a function to run when a change event occurs.
+   $('#order-id').change(function() {
+     //  val() is used to get input elements values in jQuery
+     var orderId = $("#order-id").val();
+     //  var customurl = "<?php echo $this->getUrl().'frontname/index/index'?>";
+     //  check the order is correct
+     // this is the magento end point for orders
+
+     var URL = "https://ciaranquinlan.com/rest/V1/orders/" + orderId;
+
+     if (orderId) {
+       //               must set these variables for accessing the M2 api
+       var settings = {
+         "async": true,
+         "crossDomain": true,
+         "url": URL,
+         "method": "GET",
+         "headers": {
+           // This is the Integrations authorization Access Token setup in Magento
+           // Log in to Admin and click System > Extensions > Integrations to display the Integrations page
+           "authorization": "Bearer az5t8r5a35mzbv3ssflcxfuh2g0b2ixq",
+           "cache-control": "no-cache"
+         },
+         fail: function(xhr, textStatus, errorThrown) {
+           $(".order-details").html("<div class='error'>Request has failed, Server on fire, Please try again later</div>");
+         },
+         error: function(xhr, ajaxOptions, thrownError) {
+           $(".order-details").html("<div class='error'>We are really sorry, we cant find your order!</div>");
+         }
+       }
+     }
+     // You can access the M2 api with AJAX functions and done returns values into response from the endpoint
+     // The order details will be put into the response json array
+     $.ajax(settings).done(function(response) {
+       // all the contents of the response are sent to console.  
+       console.log(response);
+       // In M2 the following api varables are use for orders: 
+       // order_id is the internal Magento order ID (database table auto increment id)
+       // increment_id is the ID which you communicate to your customer - this is what we need.
+       // quote_id is the internal quote id 
+
+       if (response.increment_id) {
+         // need to check if customer is undefined, this will happen if ordered and are not registered customers.
+         var firstName = response.customer_firstname;
+         if (firstName === undefined || firstName === null) {
+           firstName = "Guest Visitor";
+         }
+         // get the day they placed the order and reformat it.
+         var datelogCreate = new Date(response.created_at);
+         var longDateCreate = datelogCreate.toString()
+         var dayMonDateCreate = longDateCreate.substring(0, 10);
+         // get the update day they placed the order and reformat it.
+         var datelogUpdate = new Date(response.created_at);
+         var longDateUpdate = datelogUpdate.toString()
+         var dayMonDateUpdate = longDateUpdate.substring(0, 10);
+         // blot out part of the email address - next version!
+         console.log("the day of order is", dayMonDateCreate, dayMonDateUpdate);
+         $(".order-details").html(" " +
+           "Hi " + firstName + "<br>" + "Thank you for your order ref#:" + orderId + " on " + dayMonDateCreate + "." + "<br>" +
+           "Your order was updated on " + dayMonDateUpdate + " to order status of " + response.status + "." + "<br>" +
+           "We sent the details to email " + response.customer_email + "." + "<br>" +
+           "The Order total was: " + "$" + response.grand_total + "." + "<br>" +
+           "Thanks for you custom." + "<br>"
+         )
+       } else {
+         $(".order-details").html("<div class='error'>I'm really sorry your order details are missing.</div>");
+       }
+     });
+
+   });
+   $('#product-name').change(function() {
+     console.log("inproduct");
+     var productName = $("#product-name").val();
+     var URL = "https://ciaranquinlan.com/rest/V1/products/" + productName;
+     if (productName) {
+       var settings = {
+         "async": true,
+         "crossDomain": true,
+         "url": URL,
+         "method": "GET",
+         //           You can call this function with a delay image
+         //           beforeSend: function() {
+         //             $(".product .loading-image").show();
+         //           },
+         "headers": {
+           "authorization": "Bearer az5t8r5a35mzbv3ssflcxfuh2g0b2ixq",
+           "cache-control": "no-cache"
+         },
+         fail: function(xhr, textStatus, errorThrown) {
+
+           $(".product-details").html("<class='error'>Request failed, Server Down! Please try again later</div>");
+         },
+         error: function(xhr, ajaxOptions, thrownError) {
+           $(".product-details").html("<class='error'>Snap! No product found</div>");
+         }
+
+       }
+     }
+
+     $.ajax(settings).done(function(response) {
+       var desc = '';
+       var qty = '';
+       var productImage = '';
+       var productImageUrl = '';
+       console.log("full response=", response);
+
+       // Find the short description in the custom attrib array    
+       jQuery.each(response.custom_attributes, function(i, val) {
+         if (val.attribute_code === 'short_description') {
+           desc = val.value;
+           console.log("val.value=", val.value);
+         }
+       });
+       // Find the image in the custom attrib array  and add https://ciaranquinlan.com/pub/media/catalog/product/
+       jQuery.each(response.custom_attributes, function(i, val) {
+         if (val.attribute_code === 'image') {
+
+           productImage = "https://ciaranquinlan.com/pub/media/catalog/product/" + val.value;
+           console.log("image=", productImage);
+         }
+       });
+
+       // Find the stock  in the extension attrib array       
+       if (response.extension_attributes.stock_item) {
+         qty = response.extension_attributes.stock_item.qty;
+       }
+
+       if (response.id) {
+
+         $(".product-details").html(
+           "The product you searched for is : " + response.name +
+           "</br>" +
+           "The Product short description is : " + desc +
+           "</br>" +
+           "The Product cost is: " + '$' + response.price +
+           "</br>" +
+           "The product is in stock:" + qty +
+           "The image is is in stock:" + qty +
+           "</br>" +
+           "<img src='" + productImage + "' alt='this is the product you asked for' style='width:242px;height:242px;border:0;' >"
+         )
+       } else {
+         $(".order-details").html("<div class='error'>Something wrong - Result not found</div>");
+       }
+     });
+
+   });
+
+ });
